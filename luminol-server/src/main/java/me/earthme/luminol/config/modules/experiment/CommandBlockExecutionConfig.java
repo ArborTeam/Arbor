378aaa23c95eede2912be69b7c54bccb038886ec
MrHua269 <mrhua269@gmail.com>
2025-07-13 22:38:35 +0800

    Add experimental config for command block command execution

---
+++ luminol-server/src/main/java/me/earthme/luminol/config/modules/experiment/CommandBlockExecutionConfig.java
@@ -0,0 +1,21 @@
+package me.earthme.luminol.config.modules.experiment;
+
+import me.earthme.luminol.config.EnumConfigCategory;
+import me.earthme.luminol.config.IConfigModule;
+import me.earthme.luminol.config.flags.ConfigInfo;
+
+public class CommandBlockExecutionConfig implements IConfigModule {
+
+    @ConfigInfo(baseName = "enabled")
+    public static boolean enabled = false;
+
+    @Override
+    public EnumConfigCategory getCategory() {
+        return EnumConfigCategory.EXPERIMENT;
+    }
+
+    @Override
+    public String getBaseName() {
+        return "force_enable_command_block_command_execution";
+    }
+}

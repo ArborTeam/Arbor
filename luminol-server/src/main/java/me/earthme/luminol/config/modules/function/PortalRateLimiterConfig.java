f0d52ee5baa615cf47e0efee56ac2e0364880ad0
mrhua269 <mrhua269@gmail.com>
2026-02-27 12:27:41 +0800

    Mark portal rate limiters config as @HotReloadUnsupported

--- luminol-server/src/main/java/me/earthme/luminol/config/modules/function/PortalRateLimiterConfig.java
+++ luminol-server/src/main/java/me/earthme/luminol/config/modules/function/PortalRateLimiterConfig.java
@@ -3,6 +3,7 @@ package me.earthme.luminol.config.modules.function;
 import me.earthme.luminol.config.IConfigModule;
 import me.earthme.luminol.config.flags.ConfigClassInfo;
 import me.earthme.luminol.config.flags.ConfigInfo;
+import me.earthme.luminol.config.flags.HotReloadUnsupported;
 import me.earthme.luminol.enums.EnumConfigCategory;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
@@ -12,13 +13,17 @@ import org.jetbrains.annotations.Nullable;
 @ConfigClassInfo(name = "portal_rate_limit", category = EnumConfigCategory.FUNCTION)
 public class PortalRateLimiterConfig implements IConfigModule {
     @ConfigInfo(name = "enable", comments = "Whether or not to limit the portal rate when entity goes into portals")
+    @HotReloadUnsupported
     public static boolean enabled = false;
+
     @ConfigInfo(name = "maximum_portal_teleports_per_tick", comments = """
             Decides how much portal teleportation should be handled within a tick in a single tick region,when exceed,
             the portal teleportation will be pushed into the next tick
             
             Note: set to -1 to use custom expressions""")
+    @HotReloadUnsupported
     public static int maxPortalTeleportsPerTick = 200;
+
     @ConfigInfo(name = "maximum_portal_teleports_per_tick_expression", comments = """
             If the fixed limit is not enough for use, you could define your own expression to dynamically limit the
             portal rate.
@@ -28,6 +33,7 @@ public class PortalRateLimiterConfig implements IConfigModule {
                                  p (player_count)
             Example: 50 * (1 + sqrt(x/1000) + c/200 + p/5)
             """)
+    @HotReloadUnsupported
     public static String maxPortalTeleportsExpression = "50 * (1 + sqrt(e/1000) + c/200 + p/5)";
 
     // use this to prevent reallocation

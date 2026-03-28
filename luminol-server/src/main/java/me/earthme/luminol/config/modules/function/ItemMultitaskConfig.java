de9c05bc1a8e116963bdf1cc008d66e13cc1ec67
MrHua269 <mrhua269@gmail.com>
2026-03-28 09:11:12 +0800

    Move multitask config from fixes module to function module

--- luminol-server/src/main/java/me/earthme/luminol/config/modules/function/ItemMultitaskConfig.java
+++ luminol-server/src/main/java/me/earthme/luminol/config/modules/function/ItemMultitaskConfig.java
@@ -1,11 +1,11 @@
-package me.earthme.luminol.config.modules.fixes;
+package me.earthme.luminol.config.modules.function;
 
 import me.earthme.luminol.config.IConfigModule;
 import me.earthme.luminol.config.flags.ConfigClassInfo;
 import me.earthme.luminol.config.flags.ConfigInfo;
 import me.earthme.luminol.enums.EnumConfigCategory;
 
-@ConfigClassInfo(category = EnumConfigCategory.FIXES, name = "item_multitask")
+@ConfigClassInfo(category = EnumConfigCategory.FUNCTION, name = "item_multitask")
 public class ItemMultitaskConfig implements IConfigModule {
     @ConfigInfo(name = "enabled", comments = """
             Prevent the server from interrupting the state of items

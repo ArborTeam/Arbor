037066b829ea19c192dbceca7c16f25d7d2219de
MrHua269 <mrhua269@gmail.com>
2026-04-18 18:51:13 +0800

    Fix CI

--- scripts/setup_and_build.sh
+++ scripts/setup_and_build.sh
@@ -2,4 +2,4 @@
 echo "Setting up projects"
 ./gradlew --refresh-dependencies applyAllPatches
 echo "Building"
-./gradlew --refresh-dependencies createMojmapPaperclipJar
+./gradlew --refresh-dependencies createPaperclipJar

8bf914c1da6746349024cd6c9218efcd8ff713a9
MrHua269 <mrhua269@gmail.com>
2026-05-02 22:40:21 +0800

    Make IRegionCreateFunction as a FunctionalInterface

--- luminol-server/src/main/java/me/earthme/luminol/utils/IRegionCreateFunction.java
+++ luminol-server/src/main/java/me/earthme/luminol/utils/IRegionCreateFunction.java
@@ -4,6 +4,7 @@ import abomination.IRegionFile;
 
 import java.io.IOException;
 
+@FunctionalInterface
 public interface IRegionCreateFunction {
     IRegionFile create(RegionCreatorInfo info) throws IOException;
 }

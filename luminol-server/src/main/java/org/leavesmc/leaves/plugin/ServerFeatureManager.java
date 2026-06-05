ddadf2cfe85e87010ce686697b6652e2e3d8b01c
MrHua269 <mrhua269@gmail.com>
2026-06-05 09:54:31 +0800

    Fix mixin detection with leaves plugins

--- luminol-server/src/main/java/org/leavesmc/leaves/plugin/ServerFeatureManager.java
+++ luminol-server/src/main/java/org/leavesmc/leaves/plugin/ServerFeatureManager.java
@@ -19,7 +19,7 @@ public class ServerFeatureManager implements FeatureManager {
                     UPDATE_SUPPRESSION_EVENT
             ));
         }
-        if (Boolean.getBoolean("leavesclip.enable.mixin")) {
+        if (Boolean.getBoolean("leavesclip.enable.mixin") || Boolean.getBoolean("hyacinthusclip.enable.mixin")) {
             availableFeatures.add(MIXIN);
         }
     }

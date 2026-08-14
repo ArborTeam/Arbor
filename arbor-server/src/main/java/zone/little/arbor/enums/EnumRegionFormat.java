6cbcd9bc76b2013ced718aa00115d52aab1c01e3
LittleOvO <122221955@qq.com>
2026-08-14 11:05:03 +0800

    Remove support for Linear format

--- arbor-server/src/main/java/zone/little/arbor/enums/EnumRegionFormat.java
+++ arbor-server/src/main/java/zone/little/arbor/enums/EnumRegionFormat.java
@@ -1,6 +1,5 @@
 package zone.little.arbor.enums;
 
-import abomination.LinearRegionFile;
 import zone.little.arbor.config.modules.function.RegionFormatConfig;
 import zone.little.arbor.data.BufferedLinearRegionFile;
 import zone.little.arbor.utils.IRegionCreateFunction;
@@ -8,7 +7,6 @@ import net.minecraft.world.level.chunk.storage.RegionFile;
 
 public enum EnumRegionFormat {
     MCA("mca", (info) -> new RegionFile(info.info(), info.filePath(), info.folder(), info.sync())),
-    LINEAR_V2("linear", (info) -> new LinearRegionFile(info.info(), info.filePath(), info.folder(), info.sync(), RegionFormatConfig.linearCompressionLevel)),
     B_LINEAR("b_linear", (info) -> new BufferedLinearRegionFile(info.filePath(), RegionFormatConfig.linearCompressionLevel, RegionFormatConfig.blinearFlusher));
 
     private final String argument;
